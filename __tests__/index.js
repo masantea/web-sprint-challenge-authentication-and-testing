955f2744d8597dc470e425fbf772bc7d93f4fd9e
masantea <mercyansong@gmail.com>
2020-07-21 17:23:06 -0400

    completed testing and questions

--- __tests__/index.js
+++ __tests__/index.js
@@ -50,50 +50,18 @@ describe('POST /api/auth/login', () => {
     await db('users').truncate()
   })
 
-  // it('should allow a registered user to log in', async () => {
-  //   const response = await request(server)
-  //     .post('/api/auth/login')
-  //     .send({ username: 'user1', password: 'pass' })
-  //   expect(response.status).toBe(200)
-  // })
+  it('should allow a registered user to log in', async () => {
+    const response = await request(server)
+      .post('/api/auth/login')
+      .send({ username: 'user1', password: 'pass' })
+    expect(response.status).toBe(200)
+	})
 
   it('denies access if user has incorrect credentials', async () => {
     const response = await request(server)
       .post('/api/auth/login')
       .send({ username: 'user1', password: 'notmypass' })
     expect(response.status).toBe(401)
-  })
+	})
+	
 })
-
-
-
-// beforeEach(async () => {
-// 	await db.seed.run()
-// })
-
-// afterAll(async () => {
-// 	await db.destroy()
-// })
-
-// test("insert", async () => {
-// 	const res = await hobbitsModel.insert({ name: "bilbo" })
-// 	expect(res.name).toBe("bilbo")
-// })
-
-// test("findById", async () => {
-// 	const res = await userModel.findById(1)
-// 	expect(res.user).toBe("adwoa")
-// })
-
-// test("update", async () => {
-// 	const res = await hobbitsModel.update(1, { name: "bilbo" })
-// 	expect(res.name).toBe("bilbo")
-// })
-
-// test("remove", async () => {
-// 	await hobbitsModel.remove(1)
-// 	const hobbits = await db("hobbits").select()
-// 	expect(hobbits).toHaveLength(3)
-// })
-
-//module.exports = router
